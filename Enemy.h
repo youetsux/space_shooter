5ffcd85f63e9417cff914b965dd589156d444dfa
youetsux <youetsux@gmail.com>
2023-10-05 14:37:37 +0900

    敵キャラを配列にするまで

--- Enemy.h
+++ Enemy.h
@@ -1,10 +1,17 @@
 ﻿#pragma once
 #include "GameChara.h"
+
+const int EnemyInLine{ 11 };
+const int EnemyLines{ 5 };
+const int EnemyNum{ EnemyInLine * EnemyLines };
+
 class Enemy :
     public GameChara
 {
 public:
 	Enemy(Vec2 _pos);
+	Enemy();
+	void SetPosition(Vec2 _pos);
 	void Update() override;
 	void Draw() override;
 };
