27b4604121d8c9bf4c4fe447a773a2b2a8ab812e
Youetsu SATO <youetsux@gmail.com>
2023-10-13 15:17:01 +0900

    ポインタを使ってループを回す！

--- Main.cpp
+++ Main.cpp
@@ -14,23 +14,15 @@ void Main()
 	// 背景の色を設定する | Set the background color
 	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
-	//インスタンス　クラスから作った変数の事（クラスの実体）
-	//Player p;
-	
-	//GameCharaクラスのインスタンス配列にぶち込んで、UpdateとDrawで同じ動きをするように
-	//書き換える！
-	//PlayerもEnemyも作ったら、アドレスをこいつにpush_back
 	std::vector<GameChara*> objList;
-	GameChara** objList;
+	//GameChara** objList;
 
 	Player* p = nullptr;
 	Enemy* e = nullptr;
-	////何も引数書かないと、引数なしのコンストラクタが呼ばれる
+
 	p = new Player;
-	//int* arr = nullptr;
-	//arr = new int[10];
-	//PlayerもEnemyもDrawとUpdateをオーバーライドしてね
-	e = new Enemy[EnemyNum];
+	objList.push_back(p);
+
 	//インスタンス配列を動的に取得すると引数なしのコンストラクタしか呼べない
 	for(int j = 0;j<EnemyLines;j++)
 	{
@@ -39,25 +31,25 @@ void Main()
 			//(i,j)で敵の座標を調整、とりあえず端から並べる
 			Vec2 chrSize = { ENEMY_CHR_SIZE, ENEMY_CHR_SIZE };
 			Vec2 enemyPos = { i * 100, j * 80 };
-			e[j * EnemyInLine + i].SetPosition(enemyPos + chrSize);
+			//e[j * EnemyInLine + i].SetPosition();
+			e = new Enemy(enemyPos + chrSize);
+			objList.push_back(e);
 		}
 		
 	}
 
 	while (System::Update())
 	{
-		p->Update();
-		p->Draw();
-		for (int i = 0; i < EnemyNum; i++) {
-			e[i].Update();
-			e[i].Draw();
-			//(e+i)->Update();
-			//(e+i)->Draw();
-		}
-		
+		for (auto& theI : objList)
+			theI->Update();
+		for (auto& theI : objList)
+			theI->Draw();
 	}
-	delete p;
-	delete[] e;
+
+	for (auto& theI : objList)
+		delete theI;
+	objList.clear();
+	//delete[] e;
 }
 
 
