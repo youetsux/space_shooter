8db8f0d1062691f6201f7db6081e87fcf159e583
youetsux <youetsux@gmail.com>
2023-10-25 11:49:39 +0900

    爆発クラスをちょっと変更

--- ExplosionEffect.cpp
+++ ExplosionEffect.cpp
@@ -1,2 +1,13 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "ExplosionEffect.h"
+
+ExplosionEffect::ExplosionEffect(Vec2 _pos)
+	:GameChara(_pos),CDTimer_(EXPLOSION_EXPLODE_TIME)
+{
+	pos_ = _pos;
+	speed_ = 0;
+	tex_ = TextureAsset(U"EXPLOSION");
+	SetCharaRect(SizeF{ EXPLOSION_RECT_SIZE, EXPLOSION_RECT_SIZE });
+	moveDir_ = { 1, 0 };
+	isAlive_ = true;
+}
