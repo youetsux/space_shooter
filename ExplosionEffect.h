3a4e063dd94ab779f6604a98fe643f4ee02967b4
y.sato <y.sato@jc-21.jp>
2023-10-25 10:11:08 +0900

    爆発エフェクトの途中

---
+++ ExplosionEffect.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "GameChara.h"
+class ExplosionEffect :
+    public GameChara
+{
+public:
+	ExplosionEffect(double _time){}
+	~ExplosionEffect(){}
+	double CDTimer_;
+
+
+};
+
