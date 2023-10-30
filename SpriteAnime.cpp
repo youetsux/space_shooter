c9fa688391661a0b58bb75d5321a6644743b882c
youetsux <youetsux@gmail.com>
2023-10-30 14:21:31 +0900

    spriteAnime classを追加

---
+++ SpriteAnime.cpp
@@ -0,0 +1,63 @@
+﻿#include "stdafx.h"
+#include "SpriteAnime.h"
+
+SpriteAnime::SpriteAnime(Texture _tex, double _interval)
+{
+	tex_ = _tex;
+}
+
+SpriteAnime::~SpriteAnime()
+{
+}
+
+void SpriteAnime::Initialize()
+{
+}
+
+void SpriteAnime::Release()
+{
+}
+
+void SpriteAnime::Update()
+{
+	//CDTimerが<0になったら、フレームナムを１増加させ、CDTimerをリセット（ANIME_INTERVALにもどす）
+	if (timer.IsTimeOver()) {
+		frameNum = (frameNum + 1) % MAX_FRAME_NUM;
+		timer.ResetTimer();
+	}
+	timer.Update();
+}
+
+void SpriteAnime::Draw()
+{
+	tex_(frames_[frameNum]).resized(PLAYER_CHR_SIZE * 3).drawAt(pos_);
+	rect_.drawFrame(1, 1, Palette::Red);
+}
+
+bool CDTIMER::IsTimeOver()
+{
+	return(CDTimer_ < 0);
+}
+
+void CDTIMER::ResetTimer()
+{
+	CDTimer_ = ANIME_INTERVAL;
+	StartTimer();
+}
+
+void CDTIMER::StartTimer()
+{
+	isTimerRun_ = true;
+}
+
+void CDTIMER::STopTimer()
+{
+	isTimerRun_ = false;
+}
+
+void CDTIMER::Update()
+{
+	if (isTimerRun_)
+		CDTimer_ = CDTimer_ - Scene::DeltaTime();
+	//Print << CDTimer_;
+}
---
+++ SpriteAnime.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "GameChara.h"
+#include <vector>
+
+using std::vector;
+
+const SizeF PLAYER_CHR_SIZE{ 48, 48 }; //がぞうさいず
+const SizeF PLAYER_RECT_SIZE{ 48, 48 }; //当たり判定用のBBのサイズ
+const double PLAYER_MOVE_SPEED{ PLAYER_CHR_SIZE.x * 1.0 }; //プレイヤーの動作スピード
+const int MAX_FRAME_NUM{ 9 };//アニメのコマ数
+const double ANIME_INTERVAL{ 1 / 15.0 };
+
+
+class CDTIMER {
+public:
+	CDTIMER() :CDTimer_(ANIME_INTERVAL), isTimerRun_(true) {}
+	double CDTimer_;
+	bool IsTimeOver();
+	void ResetTimer();
+	void StartTimer();
+	void STopTimer();
+	bool isTimerRun_;
+	void Update();
+};
+
+class SpriteAnime :
+    public GameChara
+{
+	int frameNum;//今のアニメフレームは何フレーム目？
+public:
+	SpriteAnime(Texture _tex, double _interva);
+	~SpriteAnime();//解放処理ここでやる？
+	CDTIMER timer;
+	vector<RectF> frames_;
+	void Initialize(); //初期化用
+	void Release(); //解放用
+	void Update() override;
+	void Draw() override;
+};
+
